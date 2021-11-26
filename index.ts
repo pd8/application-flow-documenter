39b3fcc267ab39246732c4563f210ae0553af8bd
Pip Dunne <pipjdunne@gmail.com>
2021-11-26 12:39:48 +0000

    tidy file

--- index.ts
+++ index.ts
@@ -106,6 +106,19 @@ const isModuleDeclaration = (
   const n = node as Estree.ModuleDeclaration;
   return importTypes.includes(n.type) || exportTypes.includes(n.type);
 };
+const isImport = (
+  node: Estree.Directive | Estree.Statement | Estree.ModuleDeclaration
+): node is Estree.ImportDeclaration => {
+  return importTypes.includes(node.type);
+};
+const isExport = (
+  node: Estree.Directive | Estree.Statement | Estree.ModuleDeclaration
+): node is
+  | Estree.ExportAllDeclaration
+  | Estree.ExportNamedDeclaration
+  | Estree.ExportDefaultDeclaration => {
+  return importTypes.includes(node.type);
+};
 
 const isRelativePath = (path: string): boolean =>
   path.startsWith("./") || path.startsWith("../");
@@ -148,6 +161,104 @@ const getFile = async (entry: Entry): Promise<File | null> => {
   return { contents: transpiledContents, hash, entry };
 };
 
+const getFormattedImports = (
+  imports: Array<Estree.ImportDeclaration>,
+  basePath: string,
+  entry: Entry
+): SuccessfulRegistry["imports"] => {
+  return imports
+    .map((node) => {
+      const { type, source, specifiers } = node;
+      const nodePath = (source.value?.toString() || "").replace(basePath, "");
+
+      const joinedPath = isRelativePath(nodePath)
+        ? path.join(entry.relativeDirectory, nodePath)
+        : null;
+
+      const from = joinedPath || nodePath;
+
+      console.debug(from);
+
+      return specifiers.map((specifier) => {
+        switch (specifier.type) {
+          case "ImportDefaultSpecifier":
+            return {
+              name: "default",
+              type,
+              from,
+            };
+          case "ImportSpecifier":
+            return {
+              name: specifier.imported.name,
+              type,
+              from,
+            };
+          case "ImportNamespaceSpecifier":
+            return {
+              name: "all",
+              type,
+              from,
+            };
+        }
+      });
+    })
+    .flat(100);
+};
+
+const getFormattedExports = (
+  exports: Array<
+    | Estree.ExportAllDeclaration
+    | Estree.ExportNamedDeclaration
+    | Estree.ExportDefaultDeclaration
+  >
+): SuccessfulRegistry["exports"] => {
+  return (
+    exports
+      .map((node) => {
+        const { type } = node;
+        switch (node.type) {
+          case "ExportAllDeclaration":
+            return { type, name: "all" };
+          case "ExportNamedDeclaration":
+            if (node.specifiers.length) {
+              return node.specifiers.map((specifier) => {
+                return {
+                  name: specifier.exported.name,
+                  type,
+                };
+              });
+            } else if (node.declaration) {
+              if (
+                node.declaration.type === "FunctionDeclaration" &&
+                node.declaration.id
+              ) {
+                return {
+                  name: node.declaration.id.name,
+                  type,
+                };
+              } else if (node.declaration.type === "VariableDeclaration") {
+                return node.declaration.declarations.map((declaration) => {
+                  switch (declaration.id.type) {
+                    case "Identifier":
+                      return {
+                        name: declaration.id.name,
+                        type,
+                      };
+                  }
+                });
+              }
+            }
+          case "ExportDefaultDeclaration":
+            return {
+              name: "default",
+              type,
+            };
+        }
+      })
+      .filter((a) => !!a) as SuccessfulRegistry["exports"]
+  ).flat(100);
+};
+
 async function createModuleRegistry(
   files: Array<File>,
   basePath: string
@@ -171,108 +282,21 @@ async function createModuleRegistry(
             isModuleDeclaration(node)
           ) as Array<Estree.ModuleDeclaration>;
 
-        const imports: SuccessfulRegistry["imports"] = (
-          importsAndExports.filter(({ type }) =>
-            importTypes.includes(type)
-          ) as Array<Estree.ImportDeclaration>
-        )
-          .map((node) => {
-            const { type, source, specifiers } = node;
-            const nodePath = (source.value?.toString() || "").replace(
-              basePath,
-              ""
-            );
+        const imports: SuccessfulRegistry["imports"] = getFormattedImports(
+          importsAndExports.filter((node) =>
+            isImport(node)
+          ) as Array<Estree.ImportDeclaration>,
+          basePath,
+          entry
+        );
 
-            const joinedPath = isRelativePath(nodePath)
-              ? path.join(entry.relativeDirectory, nodePath)
-              : null;
-
-            const from = joinedPath || nodePath;
-
-            console.debug(from);
-
-            return specifiers.map((specifier) => {
-              switch (specifier.type) {
-                case "ImportDefaultSpecifier":
-                  return {
-                    name: "default",
-                    type,
-                    from,
-                  };
-                case "ImportSpecifier":
-                  return {
-                    name: specifier.imported.name,
-                    type,
-                    from,
-                  };
-                case "ImportNamespaceSpecifier":
-                  return {
-                    name: "all",
-                    type,
-                    from,
-                  };
-              }
-            });
-          })
-          .flat(100);
-
-        const exports: SuccessfulRegistry["exports"] = (
-          (
-            importsAndExports.filter(({ type }) =>
-              exportTypes.includes(type)
-            ) as Array<
-              | Estree.ExportAllDeclaration
-              | Estree.ExportNamedDeclaration
-              | Estree.ExportDefaultDeclaration
-            >
-          )
-            .map((node) => {
-              const { type } = node;
-              switch (node.type) {
-                case "ExportAllDeclaration":
-                  return { type, name: "all" };
-                case "ExportNamedDeclaration":
-                  if (node.specifiers.length) {
-                    return node.specifiers.map((specifier) => {
-                      return {
-                        name: specifier.exported.name,
-                        type,
-                      };
-                    });
-                  } else if (node.declaration) {
-                    if (
-                      node.declaration.type === "FunctionDeclaration" &&
-                      node.declaration.id
-                    ) {
-                      return {
-                        name: node.declaration.id.name,
-                        type,
-                      };
-                    } else if (
-                      node.declaration.type === "VariableDeclaration"
-                    ) {
-                      return node.declaration.declarations.map(
-                        (declaration) => {
-                          switch (declaration.id.type) {
-                            case "Identifier":
-                              return {
-                                name: declaration.id.name,
-                                type,
-                              };
-                          }
-                        }
-                      );
-                    }
-                  }
-                case "ExportDefaultDeclaration":
-                  return {
-                    name: "default",
-                    type,
-                  };
-              }
-            })
-            .filter((a) => !!a) as SuccessfulRegistry["exports"]
-        ).flat(100);
+        const exports: SuccessfulRegistry["exports"] = getFormattedExports(
+          importsAndExports.filter((node) => isExport(node)) as Array<
+            | Estree.ExportAllDeclaration
+            | Estree.ExportNamedDeclaration
+            | Estree.ExportDefaultDeclaration
+          >
+        );
 
         return {
           id: "id",
